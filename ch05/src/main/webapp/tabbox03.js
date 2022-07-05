2d917197bffe277e6f4584fdf636f8b14d9211b5
hwimin-kim <gnlals7841@gmail.com>
2022-07-05 16:38:29 +0900

    browser programming: jquery#6 && tabbox03

---
+++ ch05/src/main/webapp/tabbox03.js
@@ -0,0 +1,26 @@
+/*  
+    DOM Event Model1 Level0: basic: HTML Element의 
+    Event Attribute에 Event Handler(함수)를 설정 하는 방식
+    JavaScript Code(DOM API)로만 작성하기
+*/
+let liSelected = null;
+
+var tabBox = {
+    init: function() {
+        $(this._onWindowLoad.bind(this));
+    }, 
+    _onTabClicked: function () {
+        // unselect
+        var $lisSelected = $('.selected');
+        $lisSelected.removeClass('selected');
+
+        // select
+        $(this).addClass('selected');
+    },
+    _onWindowLoad: function() {
+        var $liTabs = $('.tab-box > ul > li');
+        console.log($liTabs);
+        for(var i = 0; i < $liTabs.length; i++)
+            $liTabs[i].addEventListener('click', this._onTabClicked);
+    }
+}
