781402cd1b8acffbca9e9d45b1b1a6ad1d392966
hwimin-kim <gnlals7841@gmail.com>
2022-07-11 14:51:13 +0900

    common.js

--- javascript-practices/basics/module-ex04/index.js
+++ javascript-practices/basics/module-ex04/index.js
@@ -1,3 +1,7 @@
-var mod01 = require('./error-module01');
+var mod01 = require('./error-mod01');
+var mod02 = require('./error-mod02');
+var mod03 = require('./error-mod03');
 
 console.log(mod01);
+mod02();
+console.log(mod03);
